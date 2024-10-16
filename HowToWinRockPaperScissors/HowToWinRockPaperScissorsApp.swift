e934fb6ed6497f444bedc76f6fbe270584a83686
PatrykMCoder <patrykmarciszewski3@gmail.com>
2024-10-16 18:46:08 +0200

    Initial Commit

---
+++ HowToWinRockPaperScissors/HowToWinRockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToWinRockPaperScissorsApp.swift
+//  HowToWinRockPaperScissors
+//
+//  Created by Patryk Marciszewski on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HowToWinRockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HowToWinRockPaperScissorsTests/HowToWinRockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HowToWinRockPaperScissorsTests.swift
+//  HowToWinRockPaperScissorsTests
+//
+//  Created by Patryk Marciszewski on 16/10/2024.
+//
+
+import Testing
+@testable import HowToWinRockPaperScissors
+
+struct HowToWinRockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
